71e5f8cfa7d5d0471977f23302c7f204ebe1c419
DerishaDv <dvderisha@gmail.com>
2022-08-04 16:33:21 +0530

    Added

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Calculator {
+	public void add(int a,int b) {
+		System.out.println("Sum: "+(a+b));
+	}
+	public void subract(int a,int b) {
+		System.out.println("Difference: "+(a-b));
+	}
+	public void multiply(int a,int b) {
+		System.out.println("Product: "+(a*b));
+	}
+	public void divide(int a,int b) {
+		System.out.println("Quotient"+(a/b));
+	}
+
+}
